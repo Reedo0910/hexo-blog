ade4b87398d5383dbc3fe0e07a159c41119b057a
zeee <sea.sand@live.cn>
2018-02-05 14:50:48 +0800

    add totop to index

--- themes/TKL/source/js/totop.js
+++ themes/TKL/source/js/totop.js
@@ -1,7 +1,7 @@
 (function ($) {
     // When to show the scroll link
     // higher number = scroll link appears further down the page
-    var upperLimit = 1000;
+    var upperLimit = 800;
 
     // Our scroll link element
     var scrollElem = $('#totop');
@@ -9,16 +9,11 @@
     // Scroll to top speed
     var scrollSpeed = 450;
 
-    // // Show and hide the scroll to top link based on scroll position
-    // scrollElem.hide();
-
     function isElemShow() {
         var scrollTop = $(document).scrollTop();
         if (scrollTop > upperLimit) {
-            // $(scrollElem).stop().animate({bottom:'50px'});
             $(scrollElem).css('bottom','50px');
         } else {
-            // $(scrollElem).stop().animate({bottom:'-100px'});
             $(scrollElem).css('bottom','-100px');            
         }
     }
